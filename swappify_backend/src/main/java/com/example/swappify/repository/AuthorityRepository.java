ba96cb647838cf71c717fc13175c7f40c5a12f81
krzysiek <krzysiek.kolodziejczyk@vp.pl>
2023-04-18 16:45:49 +0200

    Change module name.

--- swappify_backend/src/main/java/com/example/swappify/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.swappify.repository;
-
-import com.example.swappify.model.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-}
