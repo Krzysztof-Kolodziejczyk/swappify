526590508c122beb271d909e6e8a4b4be8838283
krzysiek <krzysiek.kolodziejczyk@vp.pl>
2023-04-18 02:49:54 +0200

    Added user authentication.

--- swappify_backend/src/test/java/com/example/swappify/SwappifyApplicationTests.java
+++ swappify_backend/src/test/java/com/example/swappify/SwappifyApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.swappify;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SwappifyApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SwappifyApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
